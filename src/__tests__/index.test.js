6034168b499185a4bdc5475296605e6da9ff2f98
Jacob Buck <buck.jacob@gmail.com>
2020-09-26 08:42:07 +0000

    Replace it function with test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -3,17 +3,17 @@ import splice from '..';
 const arr = ['foo', 'bar', 'baz', 'qux', 'quux', 'quuz'];
 const str = 'foobar';
 
-it('removes all items from index 2', () => {
+test('removes all items from index 2', () => {
   expect(splice(arr, 2)).toEqual(['foo', 'bar']);
   expect(splice(str, 2)).toEqual('fo');
 });
 
-it('removes 2 items from index 2', () => {
+test('removes 2 items from index 2', () => {
   expect(splice(arr, 2, 2)).toEqual(['foo', 'bar', 'quux', 'quuz']);
   expect(splice(str, 2, 2)).toEqual('foar');
 });
 
-it('removes 2 items from index 2, and inserts 1 item', () => {
+test('removes 2 items from index 2, and inserts 1 item', () => {
   expect(splice(arr, 2, 2, 'blep')).toEqual([
     'foo',
     'bar',
@@ -24,7 +24,7 @@ it('removes 2 items from index 2, and inserts 1 item', () => {
   expect(splice(str, 2, 2, 'blep')).toEqual('foblepar');
 });
 
-it('removes 0 items from index 2, and inserts 1 item', () => {
+test('removes 0 items from index 2, and inserts 1 item', () => {
   expect(splice(arr, 2, 0, 'blep')).toEqual([
     'foo',
     'bar',
@@ -37,7 +37,7 @@ it('removes 0 items from index 2, and inserts 1 item', () => {
   expect(splice(str, 2, 0, 'blep')).toEqual('foblepobar');
 });
 
-it('removes 2 items from index 2, and inserts 3 items', () => {
+test('removes 2 items from index 2, and inserts 3 items', () => {
   expect(splice(arr, 2, 2, 'blep', 'blah', 'boop')).toEqual([
     'foo',
     'bar',
