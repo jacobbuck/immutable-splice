e4b4958dc32da36ad2bfe992687ceaaabe095300
Jacob Buck <buck.jacob@gmail.com>
2020-09-26 08:37:17 +0000

    Replace CommonJS export with ES Module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-'use strict';
-
-module.exports = function splice(input, start, deleteCount) {
+function splice(input, start, deleteCount) {
   deleteCount = deleteCount == null ? input.length - start : deleteCount;
   var items = [].slice.call(arguments, 3);
   var output;
   return (output = input.slice(0, start)).concat
     .apply(output, items)
     .concat(input.slice(start + deleteCount));
-};
+}
+
+export default splice;
