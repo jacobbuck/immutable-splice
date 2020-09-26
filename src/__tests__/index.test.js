07be0a131d2fc9b782396793930e4928e642925d
Jacob Buck <buck.jacob@gmail.com>
2020-09-26 08:41:01 +0000

    Replace mocka with jest. Update tests for jest.

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,21 +1,20 @@
-var expect = require('expect');
-var splice = require('./');
+import splice from '..';
 
-describe('splice', function () {
-  var arr = ['foo', 'bar', 'baz', 'qux', 'quux', 'quuz'];
-  var str = 'foobar';
+describe('splice', () => {
+  const arr = ['foo', 'bar', 'baz', 'qux', 'quux', 'quuz'];
+  const str = 'foobar';
 
-  it('removes all items from index 2', function () {
+  it('removes all items from index 2', () => {
     expect(splice(arr, 2)).toEqual(['foo', 'bar']);
     expect(splice(str, 2)).toEqual('fo');
   });
 
-  it('removes 2 items from index 2', function () {
+  it('removes 2 items from index 2', () => {
     expect(splice(arr, 2, 2)).toEqual(['foo', 'bar', 'quux', 'quuz']);
     expect(splice(str, 2, 2)).toEqual('foar');
   });
 
-  it('removes 2 items from index 2, and inserts 1 item', function () {
+  it('removes 2 items from index 2, and inserts 1 item', () => {
     expect(splice(arr, 2, 2, 'blep')).toEqual([
       'foo',
       'bar',
@@ -26,7 +25,7 @@ describe('splice', function () {
     expect(splice(str, 2, 2, 'blep')).toEqual('foblepar');
   });
 
-  it('removes 0 items from index 2, and inserts 1 item', function () {
+  it('removes 0 items from index 2, and inserts 1 item', () => {
     expect(splice(arr, 2, 0, 'blep')).toEqual([
       'foo',
       'bar',
@@ -39,7 +38,7 @@ describe('splice', function () {
     expect(splice(str, 2, 0, 'blep')).toEqual('foblepobar');
   });
 
-  it('removes 2 items from index 2, and inserts 3 items', function () {
+  it('removes 2 items from index 2, and inserts 3 items', () => {
     expect(splice(arr, 2, 2, 'blep', 'blah', 'boop')).toEqual([
       'foo',
       'bar',
